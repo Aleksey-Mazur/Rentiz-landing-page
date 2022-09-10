8623938b05aa2a48cedbc4343f697f9f2927a9c9
Aleksey <1aleksey.mazur@gmail.com>
2022-09-10 12:55:43 +0300

    changed reviews content in 'reviews' section; added 'gallery' section; implemented gallery functionality

--- app/js/main.js
+++ app/js/main.js
@@ -60,3 +60,18 @@ const reviewsSlider = new Swiper('.slider-reviews', {
     prevEl: '.slider-reviews-prev',
   },
 });
+
+// Gallery
+const galleryItems = document.querySelectorAll('.gallery__item');
+
+if (galleryItems.length > 0) {
+  galleryItems.forEach(item => {
+    new Swiper(item, {
+      slidesPerView: 1,
+      autoplay: {
+        delay: 5000,
+      },
+      effect: 'fade',
+    });
+  });
+}
